be21eddd924fbb555374be04abf438af961ae127
Nesus3Ch4n <jesusechandia2@gmail.com>
2026-03-10 12:52:59 -0500

    Mas correciónes assets

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { environment } from '../../../environments/environment';
 
 interface ProjectDemo {
   summary: string;
@@ -33,20 +34,20 @@ export class ProjectsComponent {
       title: 'Proyecto Generacion Numeros Aleatorios',
       description: 'Proyecto de generacion de numeros aleatorios con Angular 17+ y FastAPI.',
       technologies: ['Angular', 'TypeScript', 'CSS', 'FastAPI', 'Oracle'],
-      image: '${environment.baseHref}assets/images/gna/gna.png',
+      image: `${environment.baseHref}assets/images/gna/gna.png`,
       liveUrl: 'https://github.com/Nesus3Ch4n/proyecto-gna-frontend',
       demo: {
         summary: 'Generacion de numeros aleatorios.',
         details:
           'Este proyecto muestra mi experiencia en Angular, diseño responsive y optimización de rendimiento para un proceso de generacion de numeros aleatorios, realizado en las practicas empresariales de la universidad Santiago de Cali.',
         images: [
-          '${environment.baseHref}assets/images/gna/gna-login.png',
-          '${environment.baseHref}assets/images/gna/gna-register.png',
-          '${environment.baseHref}assets/images/gna/gna-home.png',
-          '${environment.baseHref}assets/images/gna/gna-numbers.png',
-          '${environment.baseHref}assets/images/gna/gna.png',
-          '${environment.baseHref}assets/images/gna/gna-audit.png',
-          '${environment.baseHref}assets/images/gna/gna-roles.png',
+          `${environment.baseHref}assets/images/gna/gna-login.png`,
+          `${environment.baseHref}assets/images/gna/gna-register.png`,
+          `${environment.baseHref}assets/images/gna/gna-home.png`,
+          `${environment.baseHref}assets/images/gna/gna-numbers.png`,
+          `${environment.baseHref}assets/images/gna/gna.png`,
+          `${environment.baseHref}assets/images/gna/gna-audit.png`,
+          `${environment.baseHref}assets/images/gna/gna-roles.png`,
         ]
       }
     },
